a74d0d552575d13f72a124742e9af94a451c5e69
Felix <zellerf24@barringtonschools.org>
2022-02-11 20:28:05 -0500

    new Problems

--- ACSLContests/fibCypher.py
+++ ACSLContests/fibCypher.py
@@ -2,8 +2,17 @@ def fibCypher(num1, num2, key, msg):
     fibSequence = [num1, num2]
     for i in range(2, 20): 
         fibSequence.append(fibSequence[i - 2] + fibSequence[i - 1])
-    print(fibSequence)
+    key = ord(key)
+    out = []
+    for index, char in enumerate(msg):
+        keyMult = (key - 97 + fibSequence[index]) % 26 + 97
+        print(key)
+        print(fibSequence[index])
+        print(keyMult)
+        print()
+        out.append(ord(char) + 3 * keyMult)
+    return " ".join(map(str, out))
 
-fibCypher(1, 7, 1, 1)
+print(fibCypher(3, 7, 'h', "ACSL c2"))
 
 
