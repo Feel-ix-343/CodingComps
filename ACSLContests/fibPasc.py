81e52c4742c9eec024ab2dea5bff445b65fc0958
Felix <zellerf24@barringtonschools.org>
2022-03-18 21:48:53 -0400

    Update

--- ACSLContests/fibPasc.py
+++ ACSLContests/fibPasc.py
@@ -1,4 +1,7 @@
 triangle = [[1], [1, 1]]
+# 5 is the fib index
 for i in range(2, 5+1):
     triangle.append([1, *[triangle[i-1][j]+triangle[i-1][j-1] for j in range(1, len(triangle[i-1]))], 1])
+
+# The diagonal
 print([triangle[i][len(triangle)-1 - i] for i in range(len(triangle)-1, (len(triangle)-len(triangle)%2)//2-1, -1)])
